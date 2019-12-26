a454145d4c34ddb263bc65c4792f70816b2eb382
Yoan Blanc <yoan@dosimple.ch>
2019-12-26 17:24:32 +0100

    webpack: externalize materialize
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,11 @@ const mix = require('laravel-mix');
 mix.sass('resources/assets/sass/app.scss', 'public/css')
     .js('resources/assets/js/app.js', 'public/js')
     .copy('node_modules/materialize-css/fonts/**', 'public/fonts')
+    .webpackConfig({
+        resolve: {
+            alias: {
+                materialize: 'materialize-css/dist/js/materialize.js'
+            }
+        }
+    })
     .version(['public/css/app.css', 'public/js/app.js'])
